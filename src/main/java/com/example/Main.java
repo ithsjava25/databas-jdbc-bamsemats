ec318952e0e1cac1bc64927ff9412c743fe88a00
Mats Rönnqvist <mats.f.ronnqvist@gmail.com>
2025-12-10 13:52:43 +0100

    Start CLI setup and login verification.

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,9 +1,8 @@
 package com.example;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
 
@@ -27,11 +26,106 @@ public class Main {
         }
 
         try (Connection connection = DriverManager.getConnection(jdbcUrl, dbUser, dbPass)) {
+            runCLI(connection);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
         //Todo: Starting point for your code
     }
+
+    private void runCLI(Connection connection) throws SQLException {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Username: ");
+            String username = scanner.nextLine();
+
+            System.out.println("Password: ");
+            String password = scanner.nextLine();
+
+            if(isValidLogin(connection, username, password)) {
+                break;
+            }
+
+            System.out.println("Invalid username or password");
+            System.out.printf("0) Exit ");
+            String options = scanner.nextLine();
+            if ("0".equals(options)) {
+                return;
+            }
+        }
+
+        while (true) {
+            prinMenu();
+            String choice = scanner.nextLine();
+
+            switch (choice) {
+                case "1":
+                    listMoonMissions(connection);
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                case "5":
+                    break;
+                case "6":
+                    break;
+                case "0":
+                    return;
+                default:
+                    System.out.println("Invalid option");
+            }
+        }
+    }
+
+    private void listMoonMissions(Connection connection) throws SQLException {
+        String sql = "SELECT spacecraft FROM moon_mission ORDER BY spacecraft";
+
+        try (PreparedStatement stmt = connection.prepareStatement(sql);
+                ResultSet rs = stmt.executeQuery()) {
+            while (rs.next()) {
+                String name = rs.getString("spacecraft");
+                System.out.println(name);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Failed to list moon missions", e);
+        }
+    };
+
+    private boolean isValidLogin(Connection connection, String username, String password) {
+        String sql = "SELECT COUNT(*) FROM account WHERE name = ? AND password = ?";
+
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setString(1, username);
+            stmt.setString(2, password);
+
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    int count = rs.getInt(1);
+                    return count == 1;
+                }
+            }
+        }
+        catch (SQLException e) {
+            throw new RuntimeException("Login query failed", e);
+        }
+
+        return false;
+    }
+
+    private void prinMenu() {
+        System.out.println("1) List moon missions");
+        System.out.println("2) Get a moon mission by mission_id");
+        System.out.println("3) Count missions for a given year");
+        System.out.println("4) Create an account");
+        System.out.println("5) Update an account password");
+        System.out.println("6) Delete an account");
+        System.out.println("0) Exit");
+    }
+
 
     /**
      * Determines if the application is running in development mode based on system properties,
